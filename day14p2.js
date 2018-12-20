355a7120333d99125979bf644fab4f3c813a50ed
Sahab Yazdani <sahab.yazdani+github@gmail.com>
2018-12-20 12:09:19 -0500

    use ever so slightly less ram

--- day14p2.js
+++ day14p2.js
@@ -1,10 +1,10 @@
-// const input = '864801'
-const input = '59414'
+const input = '864801'
+// const input = '59414'
 
 const recipes = [3, 7]
 let elf1Pos = 0
 let elf2Pos = 1
-let last6 = []
+let last6 = [0, 0, 0, 0, 0, 0]
 
 let iteration = 0
 
@@ -22,7 +22,12 @@ while (last6.join('') !== input) {
     recipes.push(newScore)
   }
 
-  last6 = recipes.slice(Math.max(recipes.length - input.length, 0))
+  last6[0] = recipes[Math.max(recipes.length - input.length, 0)]
+  last6[1] = recipes[Math.max(recipes.length - input.length + 1, 0)]
+  last6[2] = recipes[Math.max(recipes.length - input.length + 2, 0)]
+  last6[3] = recipes[Math.max(recipes.length - input.length + 3, 0)]
+  last6[4] = recipes[Math.max(recipes.length - input.length + 4, 0)]
+  last6[5] = recipes[Math.max(recipes.length - input.length + 5, 0)]
 
   elf1Pos = (elf1Pos + 1 + recipes[elf1Pos]) % recipes.length
   elf2Pos = (elf2Pos + 1 + recipes[elf2Pos]) % recipes.length
