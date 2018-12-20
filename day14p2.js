ad8c1666820d849eb48cc42d897019922d1d8ace
Sahab Yazdani <sahab.yazdani+github@gmail.com>
2018-12-20 13:39:42 -0500

    day 14 p2

--- day14p2.js
+++ day14p2.js
@@ -8,26 +8,33 @@ let last6 = [0, 0, 0, 0, 0, 0]
 
 let iteration = 0
 
-while (last6.join('') !== input) {
-  iteration++
-  if (iteration % 1000 === 0) {
-    console.log(iteration)
-  }
-
-  const newScore = recipes[elf1Pos] + recipes[elf2Pos]
-  if (newScore >= 10) {
-    recipes.push(1)
-    recipes.push(newScore - 10)
-  } else {
-    recipes.push(newScore)
-  }
-
+function updateLastSix () {
   last6[0] = recipes[Math.max(recipes.length - input.length, 0)]
   last6[1] = recipes[Math.max(recipes.length - input.length + 1, 0)]
   last6[2] = recipes[Math.max(recipes.length - input.length + 2, 0)]
   last6[3] = recipes[Math.max(recipes.length - input.length + 3, 0)]
   last6[4] = recipes[Math.max(recipes.length - input.length + 4, 0)]
   last6[5] = recipes[Math.max(recipes.length - input.length + 5, 0)]
+}
+
+while (last6.join('') !== input) {
+  iteration++
+
+  const newScore = recipes[elf1Pos] + recipes[elf2Pos]
+  if (newScore >= 10) {
+    recipes.push(1)
+
+    updateLastSix()
+    if (last6.join('') === input) {
+      break
+    }
+
+    recipes.push(newScore - 10)
+  } else {
+    recipes.push(newScore)
+  }
+
+  updateLastSix()
 
   elf1Pos = (elf1Pos + 1 + recipes[elf1Pos]) % recipes.length
   elf2Pos = (elf2Pos + 1 + recipes[elf2Pos]) % recipes.length
